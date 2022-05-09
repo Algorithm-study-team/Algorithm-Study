b814443f9bfdf8e4197413a4875def7eb1ab669e
오윤진 <funnyjin710@gmail.com>
2022-05-09 23:38:22 +0900

    code: yunjin prg 42748 k번째수

---
+++ yunjin/4주차/Programmers_k번째수/k번째수.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+   public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] tempArray = new int[commands[i][1] - commands[i][0] + 1];
+
+            int a = 0;
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+                tempArray[a] = array[j];
+                a++;
+            }
+            Arrays.sort(tempArray);
+            answer[i] = tempArray[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
