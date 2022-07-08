de27dfad56a65ef73c3274ea2492cf0f437fa568
김경민 <nutmy52@naver.com>
2022-07-08 10:47:39 +0900

    code: kyeongmin prg-92341 주차요금계산

---
+++ kyeongmin/11주차/prg-92341/prg_주차요금계산_92341.py
@@ -0,0 +1,60 @@
+import math
+
+def date(x):
+    a,b = x.split(":")
+    return int(a) * 60 + int(b)
+
+def change(records):
+    d = dict()
+    
+    for r in records:
+        a,b,c = r.split()
+        
+        if d.get(b):
+            d[b].append((date(a),c))
+        else:
+            d[b] = [(date(a),c)]
+    
+    return d
+
+def go(fees,x):
+    a,b,c,d = fees
+    result = [0] * len(x)
+
+    for i,(k,v) in enumerate(x):
+        temp = 0
+        time = 0
+        check = True
+        
+        for t in v:
+            if t[1] == "IN":
+                temp = t[0]
+                check = False
+            else:
+                time += t[0]-temp
+                check = True
+        
+        if check:
+            if a < time:
+                result[i] = b + (math.ceil((time-a)/c)) * d
+            else:
+                result[i] = b
+        else:
+            r = (1439-temp)+time
+            
+            if a < r:
+                result[i] = b + (math.ceil((r - a)/c)) * d
+            else:
+                result[i] = b
+        
+    return result
+
+def solution(fees,records):
+    answer = []
+    
+    d = change(records)
+    answer = go(fees,sorted(d.items()))
+    return answer
+
+# print(solution([180, 5000, 10, 600],["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
+print(solution([1, 461, 1, 10],["00:00 1234 IN"]))
